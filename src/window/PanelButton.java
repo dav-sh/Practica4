96fdbdb63022d001fef02d255c73c15c1364934c
David <dav.llwyr@gmail.com>
2021-06-26 20:47:21 -0600

    dado funcionando

--- src/window/PanelButton.java
+++ src/window/PanelButton.java
@@ -2,13 +2,16 @@ package src.window;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
 
-public class PanelButton extends JPanel{
-
+public class PanelButton extends JPanel implements ActionListener{
+    Random rand = new Random();
+    JLabel label;
     public PanelButton(){
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         add(Box.createRigidArea(new Dimension(0,100)));
-        addLabel("###");
+        addLabel("-");
         add(Box.createRigidArea(new Dimension(0,20)));
         add(addButton("Tirar DADO"));
         setBackground(new Color(0,75,255));
@@ -17,11 +20,24 @@ public class PanelButton extends JPanel{
     private JButton addButton(String name){
         JButton btn = new JButton(name);
         btn.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        btn.addActionListener(this);
         btn.setFocusable(false);
         return btn;
     }
     private void addLabel(String name){
-        JLabel label = new JLabel(name, JLabel.CENTER);
+        label = new JLabel(name, JLabel.CENTER);
         add(label, BorderLayout.NORTH);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        label.setText(" "+getRandom());
+        
+        
+    }
+
+    private int getRandom(){
+        return rand.nextInt(11)+1;
     }
 }
