06107159e1fd0c2f24bf62a5ee50033fb0b5369c
David <dav.llwyr@gmail.com>
2021-06-27 22:22:11 -0600

    Comenzare a ordenar e implementar lo practicado

--- src/window/PanelCell.java
+++ src/window/PanelCell.java
@@ -3,37 +3,24 @@ package src.window;
 import javax.swing.*;
 import java.awt.*;
 import src.cell.Cell; 
-import java.util.*;
 
 public class PanelCell extends JPanel {
     JLabel label;
-    ArrayList<Cell> cells = new ArrayList<Cell>();
-    Random random = new Random();
-    Cell[][]celdas= new Cell[3][2];
+    int rows = 10;
+    int columns = 10;
+    Cell[][]celdas= new Cell[rows][columns];
     public PanelCell(){
         setSize(690,550);
         fillArray();
         celdas[2][1].setBackground(Color.RED);
-        //cells.get(3).setBackground(Color.RED); //prueba
     }
-    public int generaRandom() {
-        return random.nextInt(3);
-    }
+    
 
-    /* fill the panel with format*/
-    /*
+    
     public void fillArray(){
-        setLayout(new GridLayout(3,2));
-        for(int i=0;i<12;i++){
-            cells.add(new Cell(" "+i)); //add cell to array
-            this.add(cells.get(i)); //add element of the array
-        }
-    }*/
-    public void fillArray(){
-        setLayout(new GridLayout(3,2));
-        // celdas = new Cell[3][2];
-        for(int i=0;i<3;i++){ //rows
-            for(int j=0;j<2;j++){ //columns
+        setLayout(new GridLayout(rows,columns));
+        for(int i=0;i<rows;i++){ //rows
+            for(int j=0;j<columns;j++){ //columns
                 celdas[i][j] = new Cell(" "+i+","+j); //add
                 this.add(celdas[i][j]);
             }
