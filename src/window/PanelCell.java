a6c8e236178f883cdd9d06326b38a68057610066
David <dav.llwyr@gmail.com>
2021-06-27 21:20:20 -0600

    agregue un array de celdas y elimine el array list que tenia

--- src/window/PanelCell.java
+++ src/window/PanelCell.java
@@ -9,21 +9,36 @@ public class PanelCell extends JPanel {
     JLabel label;
     ArrayList<Cell> cells = new ArrayList<Cell>();
     Random random = new Random();
+    Cell[][]celdas= new Cell[3][2];
     public PanelCell(){
         setSize(690,550);
         fillArray();
-        cells.get(3).setBackground(Color.RED);
+        celdas[2][1].setBackground(Color.RED);
+        //cells.get(3).setBackground(Color.RED); //prueba
     }
     public int generaRandom() {
         return random.nextInt(3);
     }
 
     /* fill the panel with format*/
+    /*
     public void fillArray(){
         setLayout(new GridLayout(3,2));
         for(int i=0;i<12;i++){
-            cells.add(new Cell(" "+i));
-            this.add(cells.get(i));
+            cells.add(new Cell(" "+i)); //add cell to array
+            this.add(cells.get(i)); //add element of the array
         }
+    }*/
+    public void fillArray(){
+        setLayout(new GridLayout(3,2));
+        // celdas = new Cell[3][2];
+        for(int i=0;i<3;i++){ //rows
+            for(int j=0;j<2;j++){ //columns
+                celdas[i][j] = new Cell(" "+i+","+j); //add
+                this.add(celdas[i][j]);
+            }
+        }
+
     }
+
 }
