c1f756da6f34c413aab065d391a3a5ef9667575f
David <dav.llwyr@gmail.com>
2021-06-29 19:19:41 -0600

    agregue el metodo paramostrar en pantalla los datos guardados

--- src/ficherosbin/FicheroBinario.java
+++ src/ficherosbin/FicheroBinario.java
@@ -1,6 +1,10 @@
 package src.ficherosbin;
 
 import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+
+
 
 import src.player.Player;
 
@@ -17,7 +21,7 @@ public class FicheroBinario {
         File file; 
         ObjectOutputStream objectoutput;
         
-        file= new File("d:/desktop/ArrayPlayers.bin");
+        file= new File("Players.bin");
         try {
             fileoutput = new FileOutputStream(file);
             objectoutput = new ObjectOutputStream(fileoutput);
@@ -27,6 +31,8 @@ public class FicheroBinario {
                 objectoutput.writeObject(players[i]);
 
             }
+            JOptionPane.showMessageDialog(null, "Players.bin created!!!");
+
             System.out.println("Logrado");
         } catch (IOException e) {
             // TODO Auto-generated catch block
@@ -40,23 +46,43 @@ public class FicheroBinario {
         FileInputStream fileinput;
         ObjectInputStream objectinput;
 
-        file= new File("d:/desktop/ArrayPlayers.bin");
+        file= new File("Players.bin");
         try {
             fileinput = new FileInputStream(file);
             objectinput = new ObjectInputStream(fileinput);
             Object data = objectinput.readObject();
             Player arrayP = (Player)data;
+            int numberOfPlayers = Integer.parseInt(JOptionPane.showInputDialog( "Enter the number of players to see"));
             int count =0;
-            while(count<10) {
+            DefaultListModel<String> listP = new DefaultListModel<>();
+            String str;
+            while(count<numberOfPlayers) {
                 System.out.println("*************");
                 System.out.println("Name: " +arrayP.getName());
+                str = "id: "+ arrayP.getId() +"name: "+ arrayP.getName();
+                listP.addElement(str);
                 count++;
     
                 
             }
+            JList<String> list = new JList<>(listP);
+            JFrame jadd = new JFrame();
+            jadd.setBackground(new Color(0,0,20));
+            jadd.setTitle("Data Saved"); //title of game
+            jadd.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            jadd.setBounds(500, 200, 300, 550); //size and position of JFrame
+            jadd.add(list,"Center");
+            jadd.setVisible(true);
+
         }catch (Exception e) {
             System.out.println("Algo salio mal!");
+            JOptionPane.showMessageDialog(null, "There are no players");
+
         }
 
     }
+
+
+
+
 }
