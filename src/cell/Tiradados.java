ff0d9c7e6a8805a793f55014af4fc687b882e12e
David <dav.llwyr@gmail.com>
2021-06-29 20:02:32 -0600

    agregando los tipos de celda

---
+++ src/cell/Tiradados.java
@@ -0,0 +1,26 @@
+package src.cell;
+import javax.swing.*;
+import java.awt.*;
+
+public class Tiradados extends JLabel {
+    boolean penitencia;
+    public Tiradados(String name){
+        this.penitencia = false;
+        setText(name);
+        setHorizontalAlignment(0); 
+        setVerticalAlignment(0);
+        setBackground(Color.BLUE); //change color of background
+        setOpaque(true); //see the background color
+        setSize(100, 100); //set dimensions of JLabel
+        setForeground(Color.WHITE); //change color of text
+        setBorder(BorderFactory.createLineBorder(Color.BLACK, 5)); //create a new border and define color and thickness (grosor)
+        
+    }
+    public boolean getPenitencia(){
+        return penitencia;
+    }
+
+    
+
+    
+}
